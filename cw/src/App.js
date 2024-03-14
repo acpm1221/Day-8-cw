fd4e7cae4260b0d20c41e9e12e2e60d2960b779c
Anmol Kumar <anmolcricket888@gmail.com>
2024-03-14 23:00:42 +0530

    project done

---
+++ cw/src/App.js
@@ -0,0 +1,18 @@
+
+import ProgressBar from "./ProgressBar.js";
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="app">
+     
+    <ProgressBar width="45%" height="2rem" duration='200' />
+      
+    </div>
+    </>
+  )
+}
+
+export default App
